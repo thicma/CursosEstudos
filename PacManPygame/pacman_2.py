2cdc2c429c5b62bee048f366d6c08fc3f547057c
thicma <48813355+thicma@users.noreply.github.com>
2021-01-05 09:26:50 -0300

    Avanço no Pacman

--- PacManPygame/pacman_2.py
+++ PacManPygame/pacman_2.py
@@ -4,32 +4,92 @@ pygame.init()
 screen = pygame.display.set_mode((800,600), 0)
 AMARELO = (255,255,0)
 PRETO = (0,0,0)
+AZUL = (0,0,255)
+VELOCIDADE = 1
+PARADO = 0
 
+class Cenario:
+    def __init__(self, tamanho, pac):
+        self.pacman = pac
+        self.tamanho = tamanho
+        self.pontos = 0
+        self.matriz = [
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2],
+            [2, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 1, 2],
+            [2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
+    
+    def pintar_linha(self, tela, numero_linha, linha):
+        for numero_coluna, coluna in enumerate(linha):
+            pos_x = numero_coluna * self.tamanho
+            pos_y = numero_linha * self.tamanho
+            half = self.tamanho // 2
+            cor = PRETO
+            if coluna == 2:
+                cor = AZUL
+            pygame.draw.rect(tela, cor, (pos_x, pos_y, self.tamanho, self.tamanho), 0)
+            if coluna == 1:
+                pygame.draw.circle(tela, AMARELO, (pos_x + half, pos_y+half),self.tamanho //10, 0)
+    
+    def pintar_labirinto(self, tela):
+        for numero_linha, linha in enumerate(self.matriz):
+            self.pintar_linha(tela, numero_linha, linha)
+
+    def calcular_regras(self):
+        coluna_pac_quer_ir = self.pacman.coluna_intencao
+        linha_pac_quer_ir = self.pacman.linha_intencao
+        if 0 <= coluna_pac_quer_ir < 28 and 0 <= linha_pac_quer_ir < 29:
+            if self.matriz[linha_pac_quer_ir][coluna_pac_quer_ir] !=2:
+                self.pacman.aceitar_movimento()
+                if self.matriz[linha_pac_quer_ir][coluna_pac_quer_ir] == 1:
+                    self.pontos += 1
+                    self.matriz[linha_pac_quer_ir][coluna_pac_quer_ir] = 0
+                    
 class Pacman:
-    def __init__(self):
+    def __init__(self, tamanho):
         self.coluna = 1
         self.linha = 1
         self.centro_x = 400
         self.centro_y = 300
-        self.tamanho = 800//30
+        self.tamanho = tamanho
         self.raio = int(self.tamanho/2)
-        self.velocidade_x = 1
-        self.velocidade_y = 1
-
+        self.velocidade_x = 0
+        self.velocidade_y = 0
+        self.coluna_intencao = self.coluna
+        self.linha_intencao = self.linha
     
     def calcular_regras(self):
-        self.coluna += self.velocidade_x
-        self.linha += self.velocidade_y
+        self.coluna_intencao += self.velocidade_x
+        self.linha_intencao += self.velocidade_y
         self.centro_x = int(self.coluna * self.tamanho + self.raio)
         self.centro_y = int(self.linha * self.tamanho + self.raio)
-        if self.centro_x + self.raio > 800:
-            self.velocidade_x = -1
-        if self.centro_x - self.raio < 0:
-            self.velocidade_x = 1
-        if self.centro_y + self.raio > 600:
-            self.velocidade_y = -1
-        if self.centro_y - self.raio < 0:
-            self.velocidade_y = 1
+
     
     def pintar (self, tela):
         #Desenhar o corpo do Pacman
@@ -47,20 +107,62 @@ class Pacman:
         olho_y = int(self.centro_y - self.raio * 0.70)
         olho_raio = int(self.raio / 10)
         pygame.draw.circle(tela, PRETO, (olho_x, olho_y), olho_raio, 0)
+    
+    def processar_eventos(self, eventos):
+                #Captura os eventos
+        for e in eventos:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_RIGHT:
+                    self.velocidade_x = VELOCIDADE
+                elif e.key == pygame.K_LEFT:
+                    self.velocidade_x = -VELOCIDADE
+                elif e.key == pygame.K_DOWN:
+                    self.velocidade_y = VELOCIDADE
+                elif e.key == pygame.K_UP:
+                    self.velocidade_y = -VELOCIDADE
+                    
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.K_RIGHT:
+                    self.velocidade_x = PARADO
+                elif e.key == pygame.K_LEFT:
+                    self.velocidade_x = PARADO
+                elif e.key == pygame.K_DOWN:
+                    self.velocidade_y = PARADO
+                elif e.key == pygame.K_UP:
+                    self.velocidade_y = PARADO
+    def processar_eventos_mouse(self, eventos):
+        delay = 30
+        for e in eventos:
+            if e.type == pygame.MOUSEMOTION:
+                mouse_x, mouse_y = e.pos
+                self.coluna = (mouse_x - self.centro_x) / delay
+                self.linha = (mouse_y - self.centro_y) / delay
+
+    def aceitar_movimento(self):
+        self.linha = self.linha_intencao
+        self.coluna = self.coluna_intencao
+
 
 if __name__ == "__main__":
-    pacman = Pacman()
+    size= 600 // 30
+    pacman = Pacman(size)
+    cenario = Cenario(size, pacman)
 
     while True:
         #Calcular as regras
         pacman.calcular_regras()
+        cenario.calcular_regras()
         # Pintar a tela
         screen.fill(PRETO)
+        cenario.pintar_labirinto(screen)
         pacman.pintar(screen)
         pygame.display.update()
         pygame.time.delay(100)
         
         #Captura os eventos
-        for e in pygame.event.get():
+        eventos = pygame.event.get()
+        for e in eventos:
             if e.type == pygame.QUIT:
                 exit()
+        pacman.processar_eventos(eventos)
+            
